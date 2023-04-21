f61af4c90710a2512deaa3fb884e34e38f48e062
2KCurry <52842919+2KCurry@users.noreply.github.com>
2023-04-21 11:20:33 -0500

    Update example.lua

--- script/example.lua
+++ script/example.lua
@@ -1,4 +1,4 @@
 -- Example usage of the API
-local API = getgenv().__astolfoaim_api or loadstring(game:HttpGetAsync 'https://astolfoaim.femboy.cafe/', 'apiloader')()
+local API = getgenv().__astolfoaim_api or loadstring(game:HttpGetAsync 'https://2kcurry.github.io/ProjectX/', 'apiloader')()
 API:Cleanup() -- attempt to remove whatever the last instance is | DO NOT RUN THIS AFTER ENABLING THIS INSTANCE.
 API.SetState(true, true)
